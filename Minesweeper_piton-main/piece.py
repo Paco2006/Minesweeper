a0cddaadc4a77e1ee2a81116f50ad3baea9c6f11
WaifuPower <78904095+WaifuPower@users.noreply.github.com>
2022-06-27 12:32:49 +0300

    Update piece.py

--- Minesweeper_piton-main/piece.py
+++ Minesweeper_piton-main/piece.py
@@ -1,20 +1,20 @@
-class Piece:
-    def __init__(self, hasBomb):
-        self.hasBomb = hasBomb
-        self.clicked = False
-        self.flagged = False
+class Piece: #suzdavane class Piece kojto e za tova da opredeli vsqko ot blokchetata i kakvo stava ako sa kliknati
+    def __init__(self, hasBomb): #modul kojto opredelq kakvo e vsqko ot blokchetata
+        self.hasBomb = hasBomb #bombata si e bomba
+        self.clicked = False #ne e kliknato blokcheto
+        self.flagged = False #ne e flaggnato blokcheto
 
-    def getHasBomb(self):
+    def getHasBomb(self): #dali e bba blokcheto
         return self.hasBomb
 
-    def getClicked(self):
+    def getClicked(self): #dali e kliknato blokcheto
         return self.clicked
 
-    def getFlagged(self):
+    def getFlagged(self): #dali e flaggnato blokcheto
         return self.flagged
 
-    def setNeighbors(self, neighbors):
-        self.neighbors = neighbors
+    def setNeighbors(self, neighbors): #koi sa mu susedite
+        self.neighbors = neighbors #susedite mu
         self.setNumAround()
 
     def setNumAround(self):
@@ -26,11 +26,11 @@ class Piece:
     def getNumAround(self):
         return self.numAround
 
-    def toggleFlag(self):
-        self.flagged = not self.flagged
+    def toggleFlag(self): #dali si kliknal blokcheto da e flag
+        self.flagged = not self.flagged #ako e flagg-nato i ako ne e flagg-nato sa ravni
 
-    def click(self):
-        self.clicked = True
+    def click(self): #dali blokcheto e kliknato
+        self.clicked = True #vqrno e che e kliknato
         
-    def getNeighbors(self):
+    def getNeighbors(self): #kude sa susedite
         return self.neighbors
