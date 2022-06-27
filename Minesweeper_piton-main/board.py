18444330cc17eadd08ce9d2c40626b2a90170c01
WaifuPower <78904095+WaifuPower@users.noreply.github.com>
2022-06-27 12:20:27 +0300

    Update board.py

--- Minesweeper_piton-main/board.py
+++ Minesweeper_piton-main/board.py
@@ -1,71 +1,71 @@
 import random
 from piece import Piece
 
-class Board:
-    def __init__(self, size, prob):
-        self.size = size
+class Board: #tuk suzdavame class Board kojto shte otgovarq za poleto
+    def __init__(self, size, prob): #suzdavame edin modul s vsichko nujno za poleto
+        self.size = size #razmera mu
         self.prob = prob
-        self.lost = False
-        self.numClicked = 0
-        self.numNonBombs = 0
+        self.lost = False #promenliva za tovadali si zagubil
+        self.numClicked = 0 #broq na kliknatite blokcheta
+        self.numNonBombs = 0 #broq na kliknatite blokcheta koito ne sa bombi
         self.setBoard()
 
     def setBoard(self):
-        self.board = []
-        for row in range(self.size[0]):
-            row = []
-            for col in range(self.size[1]):
-                hasBomb = random.random() < self.prob
-                if not hasBomb:
-                    self.numNonBombs +=1
-                piece = Piece(hasBomb)
-                row.append(piece)
-            self.board.append(row)
+        self.board = [] #duskata e prazna
+        for row in range(self.size[0]): #za daden red
+            row = [] #reda e prazen
+            for col in range(self.size[1]): #za dadena kolona
+                hasBomb = random.random() < self.prob #tuk slagame bombite kato te sa na random i sa po-malko ot prosenta (prob) ot duskata
+                if not hasBomb: #ako blokcheto ne e bomba
+                    self.numNonBombs +=1 #nomera na blokchetata koito ne sa bomba se uvelichava
+                piece = Piece(hasBomb) #blokcheto da e bomba
+                row.append(piece) #oduljavame reda
+            self.board.append(row) #oduljavamei poleto s broq na redovete
         self.setNeighbors()
 
     def setNeighbors(self):
-        for row in range(self.size[0]):
-            for col in range(self.size[1]):
-                piece = self.getPiece((row, col))
-                neighbors = self.getListOfNeighbors((row, col))
-                piece.setNeighbors(neighbors)
+        for row in range(self.size[0]): #koj red e
+            for col in range(self.size[1]): #koq kolona e
+                piece = self.getPiece((row, col)) #blokcheto vzima koordinatite si
+                neighbors = self.getListOfNeighbors((row, col)) #koi sa susedite
+                piece.setNeighbors(neighbors) #runvame modula dolu
 
-    def getListOfNeighbors(self, index):
+    def getListOfNeighbors(self, index): #gledame koi sa susedite na nasheto blokche
         neighbors = []
-        for row in range(index[0] - 1, index[0] + 2):
-            for col in range(index[1] - 1, index[1] + 2):
-                outOfBounds = row < 0 or row >= self.size[0] or col < 0 or col>= self.size[1]
-                same = row == index[0] and col == index [1]
-                if same or outOfBounds:
-                    continue
-                neighbors.append(self.getPiece((row,col)))
+        for row in range(index[0] - 1, index[0] + 2): #oglejdame okolo nego - ref
+            for col in range(index[1] - 1, index[1] + 2): #oglejdame okolo nego - kolona
+                outOfBounds = row < 0 or row >= self.size[0] or col < 0 or col>= self.size[1] #gledame dali susedite mu sa izvun poleto(tova vaji za blokcheta koito sa "ramkata" na poleto
+                same = row == index[0] and col == index [1] #gledame dali sa ednakvi na 2 blokcheta susedite
+                if same or outOfBounds: #ako sa
+                    continue #produljava
+                neighbors.append(self.getPiece((row,col))) #oduljavame reda i kolonata na blokcheto
         return neighbors
     
-    def getSize(self):
+    def getSize(self): #razmera
         return self.size
     
-    def getPiece(self, index):
+    def getPiece(self, index): #modul za tova koe e blokcheto
         return self.board[index[0]][index[1]]
 
     def handleClick(self, piece, flag):
-        if piece.getClicked() or (not flag and piece.getFlagged()):
+        if piece.getClicked() or (not flag and piece.getFlagged()): #kakvo stava pri natisnat flag
             return
-        if flag:
+        if flag: #pri flag vrushtame nevuzmojnost za klik
             piece.toggleFlag()
-            return
+            return #te vrushta gore
         piece.click()
-        if piece.getHasBomb():
-            self.lost = True
-            return
-        self.numClicked +=1
-        if piece.getNumAround() != 0:
-            return
-        for neighbor in piece.getNeighbors():
-            if not neighbor.getHasBomb() and not neighbor.getClicked():
-                self.handleClick(neighbor, False)
+        if piece.getHasBomb(): #ako sme natisnali blok kojto e s momba
+            self.lost = True #znachi gubim
+            return #te veushta gore
+        self.numClicked +=1 #i natisnatite blokove se uvelichava s 1
+        if piece.getNumAround() != 0: #ako nqma veche kude da natisnem
+            return #te vrushta gore
+        for neighbor in piece.getNeighbors(): #za susedno blokche
+            if not neighbor.getHasBomb() and not neighbor.getClicked(): #ako susednoto blokche ne e bomba i ne e kliknato
+                self.handleClick(neighbor, False) #znashi e nevqrno natiskaneto
 
-    def getLost(self):
-        return self.lost
+    def getLost(self): #modul za tova koga gubish
+        return self.lost #gubish kogato si natisnal bomba
         
-    def getWon(self):
-        return self.numNonBombs == self.numClicked
+    def getWon(self): #modul za tova koga pechelish
+        return self.numNonBombs == self.numClicked #ako si nameril vsichki blokcheta koito ne sa mombi pechelish
