86ad56553b7dadf145b6fa2add75f5a29933bf80
WaifuPower <78904095+WaifuPower@users.noreply.github.com>
2022-06-27 12:35:38 +0300

    Update main.py

--- Minesweeper_piton-main/main.py
+++ Minesweeper_piton-main/main.py
@@ -1,9 +1,9 @@
 from game import Game
 from board import Board
 
-size = (20, 50)
-prob = 0.1
-board = Board(size, prob)
-screenSize = (1700, 800)
-game = Game(board, screenSize)
-game.run()
+size = (20, 50) #goleminata na blokcheto
+prob = 0.1 #procenta - bombi/blokcheta
+board = Board(size, prob) #goleminata na poleto i kolko bombi ima
+screenSize = (1700, 800) #goleminata na terminala/igrata
+game = Game(board, screenSize) #igrata - pole i zarmeri
+game.run() #runvame igrata
